424615c8e3720b11a74a72201d74ebf8a0bd6942
GautamR24 <rawatgautam24@protonmail.com>
2023-03-27 12:23:17 +0530

    just tested

--- news_app/src/App.js
+++ news_app/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css'
 
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  c = "test";
   render() {
     return (
       <div>
-        
+        <h1>Hello my first class based components {this.c}</h1>
       </div>
     )
   }
