277bb304f26157a9df968489d1d4d9243d5160ad
GautamR24 <rawatgautam24@protonmail.com>
2023-03-28 11:11:02 +0530

    used state to hold articles,props updated

--- news_app/src/components/NewsItem.js
+++ news_app/src/components/NewsItem.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title,description} = this.props;
+    let {title,description,imageURL,newsURL} = this.props;
     return (
       <>
+      <div className="container my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://s.abcnews.com/images/Politics/Mike-Gallagher-2-gty-gmh-230325-_1679766000667_hpMain_16x9_992.jpg" className="card-img-top" alt="..."/>
+          <img src={imageURL} className="card-img-top" alt="..."/>
           <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className="btn btn-sm btn-primary">Read more</a>
           </div>
+        </div>
       </div>
       </>
     )
