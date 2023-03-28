62b8a9b5c60848704b6865c8f398dfdce1da9c27
GautamR24 <rawatgautam24@protonmail.com>
2023-03-28 13:10:17 +0530

    used api to fetch news,made some visula changes,learnt about componentDidMount

--- news_app/src/components/NewsItem.js
+++ news_app/src/components/NewsItem.js
@@ -7,10 +7,10 @@ export class NewsItem extends Component {
       <>
       <div className="container my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageURL} className="card-img-top" alt="..."/>
+          <img src={imageURL?imageURL:"https://cdn.i-scmp.com/sites/default/files/styles/og_image_scmp_generic/public/d8/images/canvas/2023/03/27/abce0b1c-a603-4d45-a3d4-cf307bc1c22f_aff784aa.jpg?itok=vr-YUvnG&v=1679893790"} className="card-img-top" alt="..."/>
           <div className="card-body">
               <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}...</p>
+              <p className="card-text">{description}</p>
               <a href={newsURL} target="_blank" className="btn btn-sm btn-primary">Read more</a>
           </div>
         </div>
