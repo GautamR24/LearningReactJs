8614739d2b8f21c982a4645163274a2df31df9da
GautamR24 <rawatgautam24@protonmail.com>
2023-03-30 12:30:18 +0530

    added default proptypes,changed url for customized categories

--- news_app/src/components/News.js
+++ news_app/src/components/News.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
+import PropTypes from 'prop-types'
+
 const agentUrl = process.env.REACT_APP_API_NEWS_API;
 
 export class News extends Component {
+
+  static defaultProps = {
+    country : 'in',
+    pageSize : 5,
+    category: 'general'
+  }
   
+  static propTypes = {
+    country : PropTypes.string,
+    pageSize : PropTypes.number,
+    category : PropTypes.string,
+  }
     /**
      * The constructor will run only one time when the app starts
      * This constructor will set the state of variables
@@ -25,7 +38,7 @@ export class News extends Component {
    */
   async componentDidMount(){
     //this runs after render always
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${agentUrl}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${agentUrl}&pageSize=${this.props.pageSize}`;
      /**
      * setting the loading state "true", during this time the loader will be visible
      */
@@ -44,7 +57,7 @@ export class News extends Component {
 
   handleNextClick=async()=>{
     console.log("next");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${agentUrl}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${agentUrl}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
      /**
      * setting the loading state "true", during this time the loader will be visible
      */
@@ -63,7 +76,7 @@ export class News extends Component {
 
   handlePrevClick=async()=>{
     console.log("prev");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${agentUrl}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${agentUrl}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     /**
      * setting the loading state "true", during this time the loader will be visible
      */
@@ -87,7 +100,7 @@ export class News extends Component {
          * We are using (array.map) to work on each element of the article array
          */
       <div className='container my-3'>
-        <h1 className="text-center">NewsPoint top headlines</h1>
+        <h1 className="text-center" style={{margin : "35px 0px"}}>NewsPoint top headlines</h1>
         {this.state.loading && <Spinner/>}
         <div className="row">
           {/* this logic will not show the news until loading state's is false */}
@@ -97,11 +110,11 @@ export class News extends Component {
                  </div>
         })}
         </div>
-        <div className="container d-flex justify-content-between my-3">
+        {!this.state.loading && <div className="container d-flex justify-content-between my-3">
           {/* the previous button and next button will be disabled based on the conditions */}
         <button disabled={this.state.page <=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&#8701; Previous</button>
         <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &#8702;</button>
-        </div>
+        </div>}
       </div>
     )
   }
