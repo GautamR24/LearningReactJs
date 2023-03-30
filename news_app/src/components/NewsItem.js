37e9a97b16f5296b30885ba3a9cbba35b7a815f4
GautamR24 <rawatgautam24@protonmail.com>
2023-03-30 12:31:04 +0530

    removed the style element to take entire pageWidth

--- news_app/src/components/NewsItem.js
+++ news_app/src/components/NewsItem.js
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
       <>
       {/* Here we will set the vaules of the card attribures using the props passed */}
       <div className="container my-3">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
           <img src={imageURL?imageURL:"https://cdn.i-scmp.com/sites/default/files/styles/og_image_scmp_generic/public/d8/images/canvas/2023/03/27/abce0b1c-a603-4d45-a3d4-cf307bc1c22f_aff784aa.jpg?itok=vr-YUvnG&v=1679893790"} className="card-img-top" alt="..."/>
           <div className="card-body">
               <h5 className="card-title">{title}</h5>
